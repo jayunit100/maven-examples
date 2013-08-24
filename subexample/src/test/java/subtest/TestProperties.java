ab598a0a65e72fd58dbb0ec715e92bc65bb14133
jayunit100 <jayunit100@gmail.com>
2013-08-24 14:08:32 -0400

    removed code from super module

--- subexample/src/test/java/subtest/TestProperties.java
+++ subexample/src/test/java/subtest/TestProperties.java
@@ -9,12 +9,14 @@ import org.junit.Test;
 public class TestProperties {
 	@Test
 	public void testSystemProps(){
+		System.out.println("sub module javaproperty:"+System.getProperties().get("env.EXAMPLE1"));
 		Assert.assertTrue(System.getProperties().get("example1").equals("hello-maven"));
 	}
 
 	@Test
 	public void testEnv(){
 		Map m = System.getenv();
+		System.out.println("sub module shellproperty:"+m.get("EXAMPLE1"));
 		Assert.assertTrue(m.containsKey("SHELL"));
 		Assert.assertTrue(m.containsKey("PATH"));
 		Assert.assertTrue(m.containsKey("HOME"));
